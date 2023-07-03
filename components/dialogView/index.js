1e6e46be5fdbfafde1e1d3ee9331fdec9626308c
Limbo <1034935762@qq.com>
2023-07-03 16:10:23 +0800

    2023年7月3日16:10:22

--- components/dialogView/index.js
+++ components/dialogView/index.js
@@ -2,6 +2,8 @@ import UvDialogFn from "./src/dialog"
 
 export default {
 	install: function (Vue) {
+		const $UvDialog = UvDialogFn(Vue)
+		window.UvDialog = $UvDialog
 		Vue.prototype.$UvDialog = UvDialogFn(Vue)
 	}
 }
