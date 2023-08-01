e71e9d4446d4a2aa02e74561ef745b54547184c0
Limbo <1034935762@qq.com>
2023-08-01 22:48:22 +0800

    2023年8月1日22:48:22

--- example/src/views/countdown/index.vue
+++ example/src/views/countdown/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<el-button type="primary" @click="flag = !flag">toggle</el-button>
+        <h3>未来的时间</h3>
 		<UvCountDown :value="futureDeadline" v-if="flag">
 			<template #default="{ d, h, m, s }">
 				{{ d }}
@@ -9,6 +10,7 @@
 				{{ s }}
 			</template>
 		</UvCountDown>
+        <h3>过去的时间</h3>
 		<keepAlive>
 			<UvCountDown :value="lastDeadline" v-if="flag">
 				<template #default="{ d, h, m, s }">
@@ -19,6 +21,15 @@
 				</template>
 			</UvCountDown>
 		</keepAlive>
+        <h3>支持tag属性</h3>
+		<UvCountDown tag="span" :value="futureDeadline" v-if="flag">
+			<template #default="{ d, h, m, s }">
+				{{ d }}
+				{{ h }}
+				{{ m }}
+				{{ s }}
+			</template>
+		</UvCountDown>
 	</div>
 </template>
 
